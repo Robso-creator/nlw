a3b3c451f5fcffcfb7b7fae98f29ec6b9d3c8904
Pichau <contato.robsonallef@gmail.com>
2020-10-24 14:24:12 -0300

    Map function

--- public/scripts/page.orphanages.js
+++ public/scripts/page.orphanages.js
@@ -1,31 +1,30 @@
 //create map
 
-const mymap = L.map('mapid').setView([-23.2161394,-46.8273787], 15)
+const mymap = L.map("mapid").setView([-23.2161394, -46.8273787], 15);
 
 //create and add tileLayer
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(mymap);
 
 //create icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popAnchor: [170, 2]
-})
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
 //create popup overlay
 
 const popup = L.popup({
-    closeButton: false,
-    className: "map-popup",
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src=".public/images/arrow-white.svg"> </a>')
+  closeButton: false,
+  className: "map-popup",
+  minWidth: 240,
+  minHeight: 240,
+}).setContent(
+  'Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src=".public/images/arrow-white.svg"> </a>'
+);
 
 //create and add marker
 
-L
-.marker([-23.2161394,-46.8273787], { icon })
-.addTo(mymap)
-.bindPopup(popup)
+L.marker([-23.2161394, -46.8273787], { icon }).addTo(mymap).bindPopup(popup);
