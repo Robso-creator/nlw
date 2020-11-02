42be44c3e5115a12b3cb8cd6e03c0bab112909c7
Pichau <contato.robsonallef@gmail.com>
2020-11-02 10:51:10 -0300

    Updating

--- public/scripts/page.orphanages.js
+++ public/scripts/page.orphanages.js
@@ -1,10 +1,10 @@
 //create map
 
-const mymap = L.map("mapid").setView([-23.2161394, -46.8273787], 15);
+const map = L.map("mapid").setView([-23.2161394, -46.8273787], 15);
 
 //create and add tileLayer
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(mymap);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 //create icon
 const icon = L.icon({
@@ -20,11 +20,13 @@ const popup = L.popup({
   closeButton: false,
   className: "map-popup",
   minWidth: 240,
-  minHeight: 64,
+  minHeight: 240,
 }).setContent(
-  'Lar das meninas <a class="choose-orphanages" </a>'
+  'Lar das meninas <a href="orphanage.hmtl?id=1" class="choose-orphanage" <img src="./public/images/arrow-white.svg"> </a>'
 );
 
 //create and add marker
 
-L.marker([-23.2161394, -46.8273787], { icon }).addTo(mymap).bindPopup(popup);
+L.marker([-23.2161394, -46.8273787], { icon })
+.addTo(map)
+.bindPopup(popup);
