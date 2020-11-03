dca01f274d1eea4e8bf0a07243f7632b71154578
Pichau <contato.robsonallef@gmail.com>
2020-11-02 22:57:20 -0300

    Updating

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -27,3 +27,29 @@ const icon = L.icon({
 
 L.marker([-23.2161394, -46.8273787], { icon })
 .addTo(map)
+
+
+/* image gallery */
+
+
+function selectImage(event){
+  const button = event.currentTarget
+
+  // remover todas as classes .active
+  const buttons = document.querySelectorAll(".images button")
+  buttons.forEach(removeActiveClass)
+
+  function removeActiveClass (button) {
+    button.classList.remove("active")
+  }
+
+  // selecionar a imagem clicada
+  const image = button.children[0] 
+  const imageContainer = document.querySelector(".orphanage-details > img")
+
+  // atualizar o container de image
+  imageContainer.src = image.src
+
+  // adicionar a classe .active para este botão
+  button.classList.add("active")
+}
