d64895eaf0a2ebf6512418e821362aea90e8b100
Pichau <contato.robsonallef@gmail.com>
2020-12-24 02:07:26 -0300

    Adding functions and organizing data

--- public/scripts/page.orphanages.js
+++ public/scripts/page.orphanages.js
@@ -11,22 +11,35 @@ const icon = L.icon({
   iconUrl: "/images/map-marker.svg",
   iconSize: [58, 68],
   iconAnchor: [29, 68],
-  popupAnchor: [170, 2]
+  popupAnchor: [170, 2],
 });
 
-//create popup overlay
+function addMarker({ id, name, lat, lng }) {
+  //create popup overlay
 
-const popup = L.popup({
-  closeButton: false,
-  className: "map-popup",
-  minWidth: 240,
-  minHeight: 240,
-}).setContent( 
-  'Lar das meninas <a href="/orphanage?id=1" class="choose-orphanage" </a>  <img src="/images/arrow-white.svg" alt="seta">'
-);
+  const popup = L.popup({
+    closeButton: false,
+    className: "map-popup",
+    minWidth: 240,
+    minHeight: 240,
+  }).setContent(
+    `${name} <a href="/orphanage?id=${id}"> <img src="/images/arrow-white.svg" alt="seta"> </a>`
+  );
 
-//create and add marker
+  //create and add marker
 
-L.marker([-23.2161394, -46.8273787], { icon })
-.addTo(map)
-.bindPopup(popup);
+  L.marker([lat, lng], { icon }).addTo(map).bindPopup(popup);
+}
+
+const orphanagesSpan = document.querySelectorAll(".orphanages span")
+
+orphanagesSpan.forEach( span => {
+  const orphanage = {
+    id: span.dataset.id,
+    name: span.dataset.name,
+    lat: span.dataset.lat,
+    lng: span.dataset.lng,
+  };
+
+  addMarker(orphanage);
+});
